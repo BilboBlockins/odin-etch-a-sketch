6656a900a036c8fa9b7f91cd258ff402abe5fc71
brett <abretthiebert@gmail.com>
2022-01-29 14:02:45 -0500

    basic board setup

---
+++ js/main.js
@@ -0,0 +1,35 @@
+console.log("Main loaded")
+
+initSketch(24, 650);
+
+pixelList = document.querySelectorAll('.pixel')
+console.log(pixelList)
+
+function initSketch(dim, canvasSize) {
+  const grid = document.querySelector(".grid-container");
+  grid.style.width = `${canvasSize}px`;
+  grid.style.height = `${canvasSize}px`;
+  let gridData = makeGrid(0, dim);
+  console.log(gridData)
+
+  gridData.forEach((row) => {
+    row.forEach((pixel) => {
+      let newPixel = document.createElement('div');
+      newPixel.className = "pixel";
+      newPixel.style.flexBasis = `${canvasSize/dim}px`;
+      grid.appendChild(newPixel);
+    });
+  });
+}
+
+function makeGrid(initialValue, dim) {
+  let gridData = [];
+  for(let i=0; i<dim; i++) {
+    let row = new Array(dim);
+    for(let j=0; j<dim; j++) {
+      row[j] = initialValue;
+    }
+    gridData.push(row)
+  }
+  return gridData;
+}
