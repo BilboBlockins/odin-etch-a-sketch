d48a48aaba86a7eabae742c48d1dfc823adbb010
brett <abretthiebert@gmail.com>
2022-02-01 14:44:42 -0500

    mucking around

--- js/main.js
+++ js/main.js
@@ -1,39 +1,49 @@
 console.log("Main loaded")
 
+let paintColor = "#000000";
+let dim = 5;
+let canvasSize = 600;
+
 main();
 
 function main() {
-  initSketch(60, 650);
-  addPixelEvents("black");
+ 
   let colorPicker = document.querySelector("#color");
-  colorPicker.addEventListener("change", watchColorPicker, false)
+  //colorPicker.addEventListener("change", watchColorPicker, false)
+  colorPicker.addEventListener("input", watchColorPicker, false);
+  initSketch(dim, canvasSize);
+  addPixelEvents(paintColor);
 }
 
 function watchColorPicker(e) {
-  console.log(e.target.value)
+  paintColor = e.target.value;
 }
 
 function updateCanvas(color, canvasSize) {
  // pixelList.forEach(pixel => {
   	
  // });
+  
 }
 
 function addPixelEvents(color) {
   let draw = false
 
   let pixelList = document.querySelectorAll('.pixel')
-  console.log(pixelList)
+  //console.log(pixelList)
+
   
   pixelList.forEach(pixel => {
     pixel.addEventListener("mouseover", (e) => {
       if(!draw) return;
-      pixel.style.backgroundColor = color;
+      pixel.style.backgroundColor = paintColor;
+      pixelList[parseInt(pixel.id)] = paintColor;
+      console.log(parseInt(pixel.id));
     });
     pixel.addEventListener("mousedown", (e) => {
-      console.log(e, pixel)
-      pixel.style.backgroundColor = color;
-
+      pixel.style.backgroundColor = paintColor;
+      pixelList[parseInt(pixel.id)] = paintColor;
+      console.log(parseInt(pixel.id));
     });
   });
 
@@ -47,21 +57,46 @@ function addPixelEvents(color) {
 }
 
 
+//Should accept grid of colors
 function initSketch(dim, canvasSize) {
   const grid = document.querySelector(".grid-container");
   grid.style.width = `${canvasSize}px`;
   grid.style.height = `${canvasSize}px`;
-  let gridData = makeGrid(0, dim);
-  console.log(gridData)
+  
+  let pixelData = makePixelData("#FFFFFF", dim);
 
-  gridData.forEach((row) => {
+  for(let i=0;i<pixelData.length;i++){
+    let newPixel = document.createElement('div');
+    newPixel.className = "pixel";
+    newPixel.id = i;
+    newPixel.style.backgroundColor = pixelData[i];
+    newPixel.style.flexBasis = `${canvasSize/dim}px`;
+    grid.appendChild(newPixel);  	
+  }
+}
+
+function updateCanvas(pixelArray, dim, canvasSize) {
+  //clear grid
+  //const grid = document.querySelector(".grid-container");
+  //grid.innerHTML = "";
+  
+  pixelArray.forEach((row) => {
     row.forEach((pixel) => {
       let newPixel = document.createElement('div');
       newPixel.className = "pixel";
+      newPixel.style.backgroundColor = pixel;
       newPixel.style.flexBasis = `${canvasSize/dim}px`;
       grid.appendChild(newPixel);
     });
-  });
+  });	
+}
+
+function makePixelData(initialValue, dim) {
+  let pixelData  = new Array(dim*dim);
+  for(let i=0; i<dim*dim; i++) {
+    pixelData[i] = initialValue;
+  }
+  return pixelData;
 }
 
 function makeGrid(initialValue, dim) {
@@ -74,4 +109,24 @@ function makeGrid(initialValue, dim) {
     gridData.push(row)
   }
   return gridData;
+}
+
+function shadeColor(color, percent) {
+  var R = parseInt(color.substring(1,3),16);
+  var G = parseInt(color.substring(3,5),16);
+  var B = parseInt(color.substring(5,7),16);
+
+  R = parseInt(R * (100 + percent) / 100);
+  G = parseInt(G * (100 + percent) / 100);
+  B = parseInt(B * (100 + percent) / 100);
+
+  R = (R<255)?R:255;  
+  G = (G<255)?G:255;  
+  B = (B<255)?B:255;  
+
+  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+  return "#"+RR+GG+BB;
 }
