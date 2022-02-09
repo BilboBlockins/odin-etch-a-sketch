851085aa839425d4246882f3f079accc6ac8aab1
brett <abretthiebert@gmail.com>
2022-02-09 13:52:40 -0500

    working with color shading

--- js/main.js
+++ js/main.js
@@ -8,17 +8,27 @@ let shadeUp = false;
 let shadeDown = false;
 let canvasData = [];
 let loadData = [];
-const gridContainer = document.querySelector('.grid-container')
+const gridContainer = document.querySelector('.grid-container');
+const clearBtn = document.querySelector('.clearBtn');
+const downloadBtn = document.querySelector("#downloadBtn");
+const fileBtn = document.querySelector("#inputFile");
+const gridSize = document.querySelector("#grid");
+const lightenBtn = document.querySelector(".lightenBtn");
+const darkenBtn = document.querySelector(".darkenBtn");
+
+clearBtn.addEventListener("click", function() {
+  clearPixels(gridContainer);
+  initSketch(gridSize.value, canvasSize)
+  addPixelEvents(paintColor);
+});
 
 //Event listener for downloading current canvas image file
-document.querySelector("#downloadBtn")
-  .addEventListener('click', function() {
+downloadBtn.addEventListener("click", function() {
   downloadData(canvasData, "pixel_data.txt")
 });
 
 //Event listener for loading pixel art file
-document.querySelector("#inputFile")
-  .addEventListener("input", function() {
+fileBtn.addEventListener("input", function() {
   let fr = new FileReader();
   fr.onload = function(){
     loadData = JSON.parse(fr.result);
@@ -32,12 +42,21 @@ document.querySelector("#inputFile")
 });
 
 //Event listener for grid size change
-let gridSize = document.querySelector("#grid")
-  .addEventListener("change", function(e) {
+gridSize.addEventListener("change", (e) => {
   console.log(e.target.value)
   clearPixels(gridContainer);
   initSketch(e.target.value, canvasSize)
   addPixelEvents(paintColor);
+});
+
+lightenBtn.addEventListener("click", function() {
+  shadeUp = !shadeUp;
+  console.log(shadeUp)
+});
+
+darkenBtn.addEventListener("click", function() {
+  shadeDown = !shadeDown;
+  console.log(shadeDown);
 });
 
 //Main function to load drawing
@@ -92,8 +111,17 @@ function addPixelEvents(color) {
       canvasData[parseInt(pixel.id)] = paintColor;
     });
     pixel.addEventListener("mousedown", (e) => {
-      pixel.style.backgroundColor = paintColor;
-      canvasData[parseInt(pixel.id)] = paintColor;
+      console.log(pixel);
+      let currentColor = RGBToHex(pixel.style.backgroundColor);
+      if(shadeUp){
+        console.log(currentColor);
+      	pixel.style.backgroundColor = shadeColorHex(currentColor, 10);
+      } else if (shadeDown) {
+      	pixel.style.backgroundColor = shadeColorHex(currentColor, -10);
+      } else {
+        pixel.style.backgroundColor = paintColor;
+        canvasData[parseInt(pixel.id)] = paintColor; 	
+      }
     });
   });
   window.addEventListener("mousedown", function(){
@@ -146,7 +174,7 @@ function makeGrid(initialValue, dim) {
 }
 
 //Function to shade hex colors by percentage
-function shadeColor(color, percent) {
+function shadeColorRGB(color, percent) {
   var R = parseInt(color.substring(1,3),16);
   var G = parseInt(color.substring(3,5),16);
   var B = parseInt(color.substring(5,7),16);
@@ -157,13 +185,66 @@ function shadeColor(color, percent) {
 
   R = (R<255)?R:255;  
   G = (G<255)?G:255;  
-  B = (B<255)?B:255;  
-
+  B = (B<255)?B:255;
+  
   var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
   var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
   var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
 
   return "#"+RR+GG+BB;
+}
+
+//Function to shade hex colors by percentage
+function shadeColorHex(color, percent) {
+  let rgb = color.match(/\d+/g);
+  let R = rgb[0];
+  let G = rgb[1];
+  let B = rgb[2];
+  console.log(R)
+  
+  console.log(color)
+
+  //var R = parseInt(color.substring(1,3),16);
+  //var G = parseInt(color.substring(3,5),16);
+  //var B = parseInt(color.substring(5,7),16);
+
+  R = parseInt(R * (100 + percent) / 100);
+  G = parseInt(G * (100 + percent) / 100);
+  B = parseInt(B * (100 + percent) / 100);
+
+  R = (R<255)?R:255;  
+  G = (G<255)?G:255;  
+  B = (B<255)?B:255;
+  
+/*
+  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+  return "#"+RR+GG+BB;
+ */
+
+  return `rgb(${R},${G},${B})`
+}
+
+function RGBToHex(rgb) {
+  // Choose correct separator
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r,g,b)" into [r,g,b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  let r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
 }
 
 main();
