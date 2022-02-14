23512f7dbb3cdca5802cb2a6bbea9072df4f8bdd
brett <abretthiebert@gmail.com>
2022-02-14 15:16:31 -0500

    removed unnecessary funtion

--- js/main.js
+++ js/main.js
@@ -200,38 +200,8 @@ function makeGrid(initialValue, dim) {
 }
 
 //Function to shade hex colors by percentage
-function shadeColorRGB(color, percent) {
-  let rgb = color.match(/\d+/g);
-  let R = rgb[0];
-  let G = rgb[1];
-  let B = rgb[2];
-  console.log(R)
-
-  //var R = parseInt(color.substring(1,3),16);
-  //var G = parseInt(color.substring(3,5),16);
-  //var B = parseInt(color.substring(5,7),16);
-
-  R = parseInt(R * (100 + percent) / 100);
-  G = parseInt(G * (100 + percent) / 100);
-  B = parseInt(B * (100 + percent) / 100);
-
-  R = (R<255)?R:255;  
-  G = (G<255)?G:255;  
-  B = (B<255)?B:255;
-  
-  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-
-  return "#"+RR+GG+BB;
-
-  return `rgb(${R},${G},${B})`
-}
-
-//Function to shade hex colors by percentage
 function shadeColorHex(color, percent) {
-  console.log(color, "from hex fuction")
-
+ //Solid black needs to be bumped up
   if(color === "#000000") {
   	color = "#111111";
   }
@@ -255,6 +225,7 @@ function shadeColorHex(color, percent) {
   return "#"+RR+GG+BB;
 }
 
+//function to convert rgb string to hex
 function RGBToHex(rgb) {
   // Choose correct separator
   let sep = rgb.indexOf(",") > -1 ? "," : " ";
